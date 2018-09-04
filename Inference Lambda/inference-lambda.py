ff2e6a3c5e3897ae2933b9d3b357a4947431b131
mahendrabairagi <40216140+mahendrabairagi@users.noreply.github.com>
2018-09-04 06:35:11 -0400

    Update inference-lambda.py

--- Inference Lambda/inference-lambda.py
+++ Inference Lambda/inference-lambda.py
@@ -1,6 +1,10 @@
-#
-# Copyright Amazon AWS DeepLens, 2017
-#
+#*****************************************************
+#                                                    *
+# Copyright 2018 Amazon.com, Inc. or its affiliates. *
+# All Rights Reserved.                               *
+#                                                    *
+#*****************************************************
+""" A sample lambda for face detection"""
 
 import os
 import sys
@@ -10,10 +14,71 @@ from threading import Timer
 import time
 import awscam
 import cv2
-from threading import Thread
+from threading import Thread, Event
 import urllib
 import zipfile
-import mo
+import json
+import numpy as np
+
+class LocalDisplay(Thread):
+    """ Class for facilitating the local display of inference results
+        (as images). The class is designed to run on its own thread. In
+        particular the class dumps the inference results into a FIFO
+        located in the tmp directory (which lambda has access to). The
+        results can be rendered using mplayer by typing:
+        mplayer -demuxer lavf -lavfdopts format=mjpeg:probesize=32 /tmp/results.mjpeg
+    """
+    def __init__(self, resolution):
+        """ resolution - Desired resolution of the project stream """
+        # Initialize the base class, so that the object can run on its own
+        # thread.
+        super(LocalDisplay, self).__init__()
+        # List of valid resolutions
+        RESOLUTION = {'1080p' : (1920, 1080), '720p' : (1280, 720), '480p' : (858, 480)}
+        if resolution not in RESOLUTION:
+            raise Exception("Invalid resolution")
+        self.resolution = RESOLUTION[resolution]
+        # Initialize the default image to be a white canvas. Clients
+        # will update the image when ready.
+        self.frame = cv2.imencode('.jpg', 255*np.ones([640, 480, 3]))[1]
+        self.stop_request = Event()
+
+    def run(self):
+        """ Overridden method that continually dumps images to the desired
+            FIFO file.
+        """
+        # Path to the FIFO file. The lambda only has permissions to the tmp
+        # directory. Pointing to a FIFO file in another directory
+        # will cause the lambda to crash.
+        result_path = '/tmp/results.mjpeg'
+        # Create the FIFO file if it doesn't exist.
+        if not os.path.exists(result_path):
+            os.mkfifo(result_path)
+        # This call will block until a consumer is available
+        with open(result_path, 'w') as fifo_file:
+            while not self.stop_request.isSet():
+                try:
+                    # Write the data to the FIFO file. This call will block
+                    # meaning the code will come to a halt here until a consumer
+                    # is available.
+                    fifo_file.write(self.frame.tobytes())
+                except IOError:
+                    continue
+
+    def set_frame_data(self, frame):
+        """ Method updates the image data. This currently encodes the
+            numpy array to jpg but can be modified to support other encodings.
+            frame - Numpy array containing the image data tof the next frame
+                    in the project stream.
+        """
+        ret, jpeg = cv2.imencode('.jpg', cv2.resize(frame, self.resolution))
+        if not ret:
+            raise Exception('Failed to set frame data')
+        self.frame = jpeg
+
+    def join(self):
+        self.stop_request.set()
+
 
 #boto3 is not installed on device by default.
 
@@ -27,39 +92,13 @@ sys.path.append(boto_dir)
 
 import boto3
 
-# Creating a greengrass core sdk client
+iot_topic = '$aws/things/{}/infer'.format(os.environ['AWS_IOT_THING_NAME'])
 client = greengrasssdk.client('iot-data')
 
-# The information exchanged between IoT and clould has
-# a topic and a message body.
-# This is the topic that this code uses to send messages to cloud
-iotTopic = '$aws/things/{}/infer'.format(os.environ['AWS_IOT_THING_NAME'])
-
-ret, frame = awscam.getLastFrame()
-ret, jpeg = cv2.imencode('.jpg', frame)
-
-Write_To_FIFO = True
-
-class FIFO_Thread(Thread):
-    def __init__(self):
-        ''' Constructor. '''
-        Thread.__init__(self)
-
-    def run(self):
-        fifo_path = "/tmp/results.mjpeg"
-        if not os.path.exists(fifo_path):
-            os.mkfifo(fifo_path)
-        f = open(fifo_path, 'w')
-        client.publish(topic=iotTopic, payload="Opened Pipe")
-        while Write_To_FIFO:
-            try:
-                f.write(jpeg.tobytes())
-            except IOError as e:
-                continue
 
 def push_to_s3(img, index):
     try:
-        bucket_name = "BUCKET_NAME"
+        bucket_name = "Bucket Name"
 
         timestamp = int(time.time())
         now = datetime.datetime.now()
@@ -68,7 +107,6 @@ def push_to_s3(img, index):
                                                    timestamp, index)
 
         s3 = boto3.client('s3')
-      
 
         encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
         _, jpg_data = cv2.imencode('.jpg', img, encode_param)
@@ -77,104 +115,99 @@ def push_to_s3(img, index):
                                  Bucket=bucket_name,
                                  Key=key)
 
-        client.publish(topic=iotTopic, payload="Response: {}".format(response))
-        client.publish(topic=iotTopic, payload="Face pushed to S3")
+        client.publish(topic=iot_topic, payload="Response: {}".format(response))
+        client.publish(topic=iot_topic, payload="Face pushed to S3")
     except Exception as e:
         msg = "Pushing to S3 failed: " + str(e)
-        client.publish(topic=iotTopic, payload=msg)
+        client.publish(topic=iot_topic, payload=msg)
 
 def greengrass_infinite_infer_run():
+    """ Entry point of the lambda function"""
     try:
-        modelType = "ssd"
-        model_name = "model"
-        input_width = 300
+        # This face detection model is implemented as single shot detector (ssd).
+        model_type = 'ssd'
+        output_map = {1: 'face'}
+        # Create a local display instance that will dump the image bytes to a FIFO
+        # file that the image can be rendered locally.
+        local_display = LocalDisplay('480p')
+        local_display.start()
+        # The sample projects come with optimized artifacts, hence only the artifact
+        # path is required.
+        model_path = '/opt/awscam/artifacts/mxnet_deploy_ssd_FP16_FUSED.xml'
+        # Load the model onto the GPU.
+        client.publish(topic=iot_topic, payload='Loading face detection model')
+        model = awscam.Model(model_path, {'GPU': 1})
+        client.publish(topic=iot_topic, payload='Face detection model loaded')
+        # Set the threshold for detection
+        detection_threshold = 0.25
+        # The height and width of the training set images
         input_height = 300
-        modelPath = "/opt/awscam/artifacts/mxnet_deploy_ssd_FP16_FUSED.xml"
-        #error, modelPath = mo.optimize(model_name,input_width,input_height)
-
-        prob_thresh = 0.25
-        results_thread = FIFO_Thread()
-        results_thread.start()
-
-        # Send a starting message to IoT console
-        client.publish(topic=iotTopic, payload="Face detection starts now")
-
-        # Load model to GPU (use {"GPU": 0} for CPU)
-        mcfg = {"GPU": 1}
-        model = awscam.Model(modelPath, mcfg)
-        client.publish(topic=iotTopic, payload="Model loaded")
-        ret, frame = awscam.getLastFrame()
-        if ret == False:
-            raise Exception("Failed to get frame from the stream")
-
-        yscale = float(frame.shape[0]/input_height)
-        xscale = float(frame.shape[1]/input_width)
-
-        doInfer = True
-        while doInfer:
+        input_width = 300
+        # Do inference until the lambda is killed.
+        while True:
             # Get a frame from the video stream
             ret, frame = awscam.getLastFrame()
-            # Raise an exception if failing to get a frame
-            if ret == False:
-                raise Exception("Failed to get frame from the stream")
+            if not ret:
+                raise Exception('Failed to get frame from the stream')
+            # Resize frame to the same size as the training set.
+            frame_resize = cv2.resize(frame, (input_height, input_width))
+            # Run the images through the inference engine and parse the results using
+            # the parser API, note it is possible to get the output of doInference
+            # and do the parsing manually, but since it is a ssd model,
+            # a simple API is provided.
+            parsed_inference_results = model.parseResult(model_type,
+                                                         model.doInference(frame_resize))
+            # Compute the scale in order to draw bounding boxes on the full resolution
+            # image.
+            yscale = float(frame.shape[0]/input_height)
+            xscale = float(frame.shape[1]/input_width)
+            # Dictionary to be filled with labels and probabilities for MQTT
+            cloud_output = {}
 
-            # Resize frame to fit model input requirement
-            frameResize = cv2.resize(frame, (input_width, input_height))
+            # Get the detected faces and probabilities
+            for i, obj in enumerate(parsed_inference_results[model_type]):
+                if obj['prob'] > detection_threshold:
+                    # Add bounding boxes to full resolution frame
+                    xmin = int(xscale * obj['xmin']) \
+                           + int((obj['xmin'] - input_width/2) + input_width/2)
+                    ymin = int(yscale * obj['ymin'])
+                    xmax = int(xscale * obj['xmax']) \
+                           + int((obj['xmax'] - input_width/2) + input_width/2)
+                    ymax = int(yscale * obj['ymax'])
 
-            # Run model inference on the resized frame
-            inferOutput = model.doInference(frameResize)
+                    if ymin >25:
+                       ymin = ymin - 25
 
-            # Output inference result to the fifo file so it can be viewed with mplayer
-            parsed_results = model.parseResult(modelType, inferOutput)['ssd']
-            # client.publish(topic=iotTopic, payload = json.dumps(parsed_results))
-            label = '{'
-            for i, obj in enumerate(parsed_results):
-                if obj['prob'] < prob_thresh:
-                    break
-                offset = 25
-                xmin = int( xscale * obj['xmin'] ) + int((obj['xmin'] - input_width/2) + input_width/2)
-                ymin = int( yscale * obj['ymin'] )
-                xmax = int( xscale * obj['xmax'] ) + int((obj['xmax'] - input_width/2) + input_width/2)
-                ymax = int( yscale * obj['ymax'] )
+               	    if ymax <275:
+                       ymax = ymax + 25
 
-		if ymin >25:
-                   ymin = ymin - 25
-           
-                if ymax <275:
-                   ymax = ymax + 25
-           
-                if xmin > 25:
+               	    if xmin > 25:
 	               xmin = xmin - 25
-                
-                if xmax <275:
-                   xmax = xmax + 25
-                
-                crop_img = frame[ymin:ymax, xmin:xmax]
 
-                push_to_s3(crop_img, i)
+                    if xmax <275:
+                       xmax = xmax + 25
 
-                cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 165, 20), 4)
-                label += '"{}": {:.2f},'.format(str(obj['label']), obj['prob'] )
-                label_show = '{}: {:.2f}'.format(str(obj['label']), obj['prob'] )
-                cv2.putText(frame, label_show, (xmin, ymin-15),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 165, 20), 4)
-            label += '"null": 0.0'
-            label += '}'
-            client.publish(topic=iotTopic, payload=label)
-            global jpeg
-            ret, jpeg = cv2.imencode('.jpg', frame)
+                    crop_img = frame[ymin:ymax, xmin:xmax]
 
-    except Exception as e:
-        msg = "Test failed: " + str(e)
-        client.publish(topic=iotTopic, payload=msg)
+                    push_to_s3(crop_img, i)
 
-    # Asynchronously schedule this function to be run again in 15 seconds
-    Timer(15, greengrass_infinite_infer_run).start()
-
+                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 165, 20), 10)
+                    # Amount to offset the label/probability text above the bounding box.
+                    text_offset = 15
+                    cv2.putText(frame, '{:.2f}%'.format(obj['prob'] * 100),
+                                (xmin, ymin-text_offset),
+                                cv2.FONT_HERSHEY_SIMPLEX, 2.5, (255, 165, 20), 6)
+                    # Store label and probability to send to cloud
+                    cloud_output[output_map[obj['label']]] = obj['prob']
+            	    # Set the next frame in the local display stream.
+            	    local_display.set_frame_data(frame)
+            	    # Send results to the cloud
+            	    client.publish(topic=iot_topic, payload=json.dumps(cloud_output))
+    except Exception as ex:
+        client.publish(topic=iot_topic, payload='Error in face detection lambda: {}'.format(ex))
 
 # Execute the function above
 greengrass_infinite_infer_run()
-
 
 # This is a dummy handler and will not be invoked
 # Instead the code above will be executed in an infinite loop for our example
